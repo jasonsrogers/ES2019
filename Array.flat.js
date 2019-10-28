a0b04d592fd2cde7463cea3852c50655e7ea5033
jasonsrogers <47950400+jasonsrogers@users.noreply.github.com>
2019-10-28 12:01:30 +0000

    Comments error correction

--- Array.flat.js
+++ Array.flat.js
@@ -1,10 +1,10 @@
-function stringifyParams(strings, ...keys) {
-  var result = [strings[0]];
-  keys.forEach(function(key, i) {
-    result.push(JSON.stringify(keys[i]), strings[i + 1]);
-  });
-  return result.join("");
-}
+  function stringifyParams(strings, ...keys) {
+    var result = [strings[0]];
+    keys.forEach(function(key, i) {
+      result.push(JSON.stringify(keys[i]), strings[i + 1]);
+    });
+    return result.join("");
+  }
 
 console.log("Array.flat.js");
 
@@ -38,7 +38,8 @@ var arr6 = [
   [{ a: 2 }, { a: 3, subArray: [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]] }]
 ];
 var arr6res = arr6.flat(Infinity);
-// [1, 2, 4, 5]
+// Arr: [{"a":1},[{"a":2},{"a":3,"subArray":[1,2,[3,4,[5,6,[7,8,[9,10]]]]]}]], arr.flat: [{"a":1},{"a":2},{"a":3,"subArray":[1,2,[3,4,[5,6,[7,8,[9,10]]]]]}]
+// can flattern with objects and flat does not affect arrays inside the object
 console.log(stringifyParams`Arr: ${arr6}, arr.flat: ${arr6res}`);
 
 
